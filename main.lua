9fccf514477865d27d7da302cdedee42cf5e3ce9
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-09 16:45:14 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -104,7 +104,7 @@ local RandomPlayer = players[math.random(1, #players)]
 repeat RandomPlayer = players[math.random(1, #players)] until RandomPlayer ~= game.Players.LocalPlayer and RandomPlayer.Character:FindFirstChild("entered") and RandomPlayer.Character:FindFirstChild("rock") == nil
 Target = RandomPlayer
 game:GetService("ReplicatedStorage").GeneralAbility:FireServer(Target.Character)
-wait(0.01)
+wait(3)
 end
 	 end
 })
