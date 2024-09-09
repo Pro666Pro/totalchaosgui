5390010da3c6c9d81ab9ab4327d85a7b2149a84c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-09 11:53:42 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ if game.PlaceId == 6403373529 or game.PlaceId == 11520107397 or game.PlaceId == 
 local GameName = "total chaos gui - by nexer"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "total chaos gui :0", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "total chaos gui :>", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
@@ -71,10 +71,6 @@ Scripts:AddButton({
       fireclickdetector(workspace.Lobby["Fort"].ClickDetector)
       wait(0.01)
       game:GetService("ReplicatedStorage").Fortlol:FireServer()
-      wait(0.5)
-      fireclickdetector(workspace.Lobby["Stun"].ClickDetector)
-      wait(0.01)
-      game:GetService("ReplicatedStorage").StunR:FireServer(game:GetService("Players").LocalPlayer.Character.Stun)
       wait(0.5)
       fireclickdetector(workspace.Lobby["Baller"].ClickDetector)
       wait(0.01)
