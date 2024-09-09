86f15887cc57c8f68a00ea19c05b8bf7ab67852f
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-09 12:16:07 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ if game.PlaceId == 6403373529 or game.PlaceId == 11520107397 or game.PlaceId == 
 local GameName = "total chaos gui - by nexer"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "total chaos gui >:]", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "total chaos gui >:0", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
@@ -73,7 +73,7 @@ Scripts:AddButton({
 })
 
 _G.OnAbility = false
-Tab7:AddToggle({
+Scripts:AddToggle({
 	Name = "make it rain",
 	Default = false,
 	Callback = function(Value)
